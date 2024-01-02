93ef1f9c6987ee0028bfd8e21748edcd4b1ec2c3
Majo <133284138+Majubi@users.noreply.github.com>
2024-01-02 16:23:02 -0300

    Add files via upload

---
+++ Clase 1/index.js
@@ -0,0 +1,9 @@
+/* Creo un prompt que soicite datos al usuario */
+
+let salida1 = prompt("Complete su nombre y apellido");
+console.log(salida1); 
+
+let salida2 = prompt("Complete con su edad");
+console.log(salida2);
+
+let nombreEdad = alert(`Soy ${salida1} y tengo ${salida2} años`);
